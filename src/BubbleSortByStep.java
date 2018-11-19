65b5567804fccf2fbfb8c083ad2f22aad19ab90c
sonminh162 <32251649+sonminh162@users.noreply.github.com>
2018-11-19 17:27:51 +0700

    finished

---
+++ src/BubbleSortByStep.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter "+list.length+" values:");
+        for(int i = 0; i<list.length;i++){
+            list[i] = scanner.nextInt();
+        }
+        System.out.println("Your input list:");
+        for(int i =0;i<list.length;i++){
+            System.out.println(list[i]);
+        }
+        System.out.println("Begin sort processing...");
+        bubbleSortByStep(list);
+    }
+    public static void bubbleSortByStep(int[] list){
+        boolean needNextPass = true;
+        for(int k = 1; k <list.length && needNextPass;k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+            if (needNextPass == false) {
+                System.out.println("Array may be sorted and next pass not needed");
+                break;
+            }
+            System.out.println("List after the " +k+"' sort:");
+            for(int i = 0;i<list.length;i++){
+                System.out.println(list[i]);
+            }
+            System.out.println();
+        }
+    }
+}
